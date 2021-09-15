8be4a011bbfe4d6fa316725d43d665a4d21901c5
Dixit Solanki <dixit.developerr@gmail.com>
2021-09-15 11:56:21 +0530

    first commit

---
+++ src/NextGrid/tables/iframe.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+
+function IframePage() {
+  return (
+    <iframe src="https://cases.stemmons.com//ViewCase.aspx?CaseID=89465" style={{'width':'100%','height':'100%'}}></iframe>
+  );
+}
+export default IframePage;
